015fa7f1233043ac109012185eac4098577bdb5c
vieveks <vivekp.padman@gmail.com>
2024-04-02 22:41:37 +0530

    added tutorials

--- minigpt/bigram.py
+++ minigpt/bigram.py
@@ -1,3 +1,5 @@
+# Inspired from Andrej Karpathy bigram model 
+
 import jax
 import jax.numpy as jnp
 from jax import random, jit, grad
@@ -18,6 +20,7 @@ learning_rate = 1e-2
 key = random.PRNGKey(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
